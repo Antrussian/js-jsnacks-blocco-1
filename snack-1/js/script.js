5e959e4497e717bccfc74d2f585994f47fa08dc3
Antrussian <antonino.russo.agent@gmail.com>
2023-11-10 11:51:46 +0100

    Dichiarazione const + promt

---
+++ snack-1/js/script.js
@@ -0,0 +1,12 @@
+/*chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande*/
+
+
+const etaPersona1 = parseInt (prompt ("inserisci l'eta della prima persona"));
+
+
+const etaPersona2 = parseInt (prompt ("inserisci l'eta della seconda persona"));
+
+
+
+
+document.getElementById('output').innerHTML = 'Ecco le tue credenziali ' + userInfo + password ;
