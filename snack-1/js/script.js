bc3d44f27234079c9eea26ebac41375818d33bcd
Antrussian <antonino.russo.agent@gmail.com>
2023-11-12 20:16:48 +0100

    Bugfix - var risultato -  stampa risultato

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,26 +1,34 @@
 /*chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande*/
 
 
-const etaPersona1 = parseInt (prompt ("inserisci l'eta della prima persona"));
-
-
-const etaPersona2 = parseInt (prompt ("inserisci l'eta della seconda persona"));
 
 
 
-if (etaPersona1 > etaPersona2) {
-    console.log(etaPerson1);
+
+
+
+const etaPersona1 = parseInt(prompt('inserisci l eta della prima persona'));
+
+
+const etaPersona2 = parseInt(prompt('inserisci l eta della seconda persona'));
+
+let risultato;
+
+if ( etaPersona1 > etaPersona2) {
+    risultato = etaPersona1;
 }
 
-else if ( etaPersona 2 > etaPersona1) {
-    console.log(etaPersona2)
+else if ( etaPersona2 > etaPersona1) {
+    risultato = etaPersona2;
 }
 
-else  (etaPersona1 == etaPersona2) {
-    console.log ("L'eta è uguale")
+else  {
+    risultato = 'L eta è uguale';
 }
 
 
 
+document.getElementById('output').innerHTML = 'Risultato = ' + risultato;
 
-document.getElementById('output').innerHTML = ;
+
+
