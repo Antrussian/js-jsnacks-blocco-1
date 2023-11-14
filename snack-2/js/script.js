00eb60c5a04a154548f66e891efb13ae45d8f014
Antrussian <antonino.russo.agent@gmail.com>
2023-11-14 12:08:44 +0100

    Primo step stampo un numero

---
+++ snack-2/js/script.js
@@ -0,0 +1,25 @@
+/*
+Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
+
+collego la pag
+
+scrivo un numero
+
+stampo
+
+aggiungo nuovo numero
+
+do proprietà alle classi
+
+*/
+
+
+const numero= 0;
+
+
+
+output.innerHTML = numero;
+
+
+
+
