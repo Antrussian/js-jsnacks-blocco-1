74909093d9c1b8af3016586440f6ae2712bc30ea
Antrussian <antonino.russo.agent@gmail.com>
2023-11-14 12:37:49 +0100

    Nuova cartella snack 3

---
+++ snack-3/js/script.js
@@ -0,0 +1,30 @@
+/*
+Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
+
+collego la pag
+
+scrivo un numero
+
+stampo
+
+aggiungo nuovo numero
+
+do proprietà alle classi
+
+*/
+
+
+
+
+const ouputElement = document.querySelector('div.outputElement')
+
+for (let output = 0; output <6; output++) {
+
+
+console.log(output);
+
+
+
+
+}
+
